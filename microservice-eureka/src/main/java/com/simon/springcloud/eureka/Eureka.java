875f2fc18b6cea8379378b748e9d917caa11b10e
u-simon <u_simon@163.com>
2019-05-03 11:49:32 +0800

    eureka-server

---
+++ microservice-eureka/src/main/java/com/simon/springcloud/eureka/Eureka.java
@@ -0,0 +1,17 @@
+package com.simon.springcloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author simon
+ * @Date 2019-05-03 11:43
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Eureka {
+	public static void main(String[] args) {
+		SpringApplication.run(Eureka.class, args);
+	}
+}
